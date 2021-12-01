014fec62098e7b1ff9291e75957d40a4d9315c97
Dima Lazarev <dmitry.lazarev@gmail.com>
2021-12-01 07:55:55 +0200

    Solve Day 1
    Solution for both parts of Day 1: Sonar Sweep.

--- src/day01.rs
+++ src/day01.rs
@@ -2,11 +2,37 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use std::num::ParseIntError;
 
 #[aoc_generator(day1)]
-fn parse_input(input: &str) -> Result<Vec<i32>, ParseIntError> {
+fn parse_input(input: &str) -> Result<Vec<u32>, ParseIntError> {
     input.lines().map(|l| l.parse()).collect()
 }
 
 #[aoc(day1, part1)]
-fn part1(entries: &[i32]) -> Option<i32> {
-    None
+fn part1(entries: &[u32]) -> usize {
+    entries.windows(2).filter(|&x| x[1] > x[0]).count()
+}
+
+#[aoc(day1, part2)]
+fn part2(entries: &[u32]) -> usize {
+    entries.windows(4).filter(|&x| x[3] > x[0]).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(
+            part1(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+            7
+        );
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(
+            part2(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+            5
+        );
+    }
 }
